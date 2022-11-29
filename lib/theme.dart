9e608d11c9d6f043655bf4d4d6cbd6c4e78ae531
fahiem152 <amfahiem010502@gmail.com>
2022-11-29 18:59:02 +0700

    slicing-supllier-portal

--- lib/theme.dart
+++ lib/theme.dart
@@ -7,6 +7,6 @@ Color whiteColor = const Color(0xffFFFFFF);
 Color weightPrimaryColor = const Color(0xffCCDAFC);
 Color successColor = const Color(0xff00D85B);
 Color primaryColor = const Color(0xff6E5DE7);
-Color backgrounColor1 = const Color(0xffF5F5F5);
+Color backgrounColor1 = const Color(0xffF9F9FB);
 
 TextStyle textOpenSans = GoogleFonts.openSans();
