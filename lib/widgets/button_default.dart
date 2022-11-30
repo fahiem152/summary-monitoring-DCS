919e90692e981c01f09e1f0987daf70fb4859fe6
rezakurniasetiawan <rezakurniasetiawan@gmail.com>
2022-11-30 09:59:57 +0700

    add Get Started and LoginPage

---
+++ lib/widgets/button_default.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:monitoring_mobile/theme.dart';
+
+class ButtonCustom extends StatelessWidget {
+  const ButtonCustom({Key? key, required this.title, required this.press}) : super(key: key);
+
+  final String title;
+  final VoidCallback press;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: GestureDetector(
+      onTap: press,
+      child: Container(
+        width: MediaQuery.of(context).size.width * 0.9,
+        height: 55,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(12),
+          color: primaryColor,
+        ),
+        child: Center(
+          child: Text(
+            title,
+            style: textOpenSans.copyWith(
+              color: whiteColor,
+              fontSize: 18,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+        ),
+      ),
+    ));
+  }
+}
