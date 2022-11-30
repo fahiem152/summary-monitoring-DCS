7f0db66cc8d8ce349c576d142cc368b974186b21
rezakurniasetiawan <rezakurniasetiawan@gmail.com>
2022-11-30 19:11:50 +0700

    Auth Services

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -2,34 +2,37 @@ import 'dart:convert';
 
 import 'package:monitoring_mobile/models/api_response_model.dart';
 import 'package:http/http.dart' as http;
+import 'package:monitoring_mobile/models/user_model.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import '../constan.dart';
 
-String apiLogin = baseURL + '/api/users/login';
+String apiLogin = 'https://dummyjson.com/auth/login';
 
 Future<ApiResponse> login(
-    {required String email, required String password}) async {
+    {required String username, required String password}) async {
   ApiResponse apiResponse = ApiResponse();
   try {
-    final response = await http.post(Uri.parse(apiLogin), headers: {
-      'Accept': 'application/json',
-      'X-Requested-With': 'XMLHttpRequest',
-    }, body: {
-      'email': email,
-      'password': password
-    });
+    final response = await http.post(
+      Uri.parse(apiLogin),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: jsonEncode(
+        {"username": username, "password": password},
+      ),
+    );
     switch (response.statusCode) {
       case 200:
         print(response.body);
-        // apiResponse.data = UserModel.fromJson(jsonDecode(response.body));
+        apiResponse.data = UserModel.fromJson(jsonDecode(response.body));
         break;
       case 422:
         final errors = jsonDecode(response.body)['data']['errors'];
         apiResponse.error = errors[errors.keys.elementAt(0)][0];
         break;
-      case 403:
-        apiResponse.error = jsonDecode(response.body)['data']['errors'];
+      case 400:
+        apiResponse.error = jsonDecode(response.body)['message'];
         break;
       default:
         apiResponse.error = somethingWhentWrong;
@@ -40,5 +43,3 @@ Future<ApiResponse> login(
   }
   return apiResponse;
 }
-
-
