253746ca1fd0c54d11fb31c3d47c606c785108e3
Taufik Nurrahman <official.taufik980@gmail.com>
2022-12-22 10:40:52 +0700

    update date

--- lib/services/mivo_service.dart
+++ lib/services/mivo_service.dart
@@ -37,4 +37,35 @@ class ServiceMivo {
       throw Exception('Show dialog for other errors');
     }
   }
+
+  static Future<List<DataMivo>> getDateDataMivo(String token,
+      {required String date}) async {
+    final response = await http.get(
+      Uri.parse(_baseInOut + '?date=$date'),
+      headers: {
+        HttpHeaders.contentTypeHeader: 'aplication/json',
+        'Authorization': 'Bearer $token',
+      },
+    );
+    if (response.statusCode == 200) {
+      // If the server did return a 200 OK response,
+      // then parse the JSON.
+      var jsonObject = jsonDecode(response.body);
+      print(jsonObject);
+      List data = (jsonObject as Map<String, dynamic>)["list"];
+      List<DataMivo> dataMivo = [];
+      dataMivo = mivoModelFromJson(data);
+      return dataMivo;
+    } else if (response.statusCode == 401) {
+      throw Exception('Show dialog $unauthorized');
+    } else if (response.statusCode == 404) {
+      throw Exception('Show dialog 404 Not Found');
+    } else if (response.statusCode == 503) {
+      throw Exception('Show dialog for error 503');
+    } else {
+      // If the server did not return a 200 OK response,
+      // then throw an exception.
+      throw Exception('Show dialog for other errors');
+    }
+  }
 }
